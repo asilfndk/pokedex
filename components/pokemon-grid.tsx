b7e886f6a6afd04479b0f9a1de142eb04f41de2c
asilfndk <fndkasil@gmail.com>
2024-01-11 05:12:00 +0300

    .

---
+++ components/pokemon-grid.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { PokemonCard } from "./pokemon-card";
+import { useState } from "react"
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+
+interface PokemonGridProps {
+    pokemonList: any
+}
+
+export function PokemonGrid({pokemonList} : PokemonGridProps) { 
+    const [searchText, setSearchText] = useState("");
+
+    console.log(pokemonList);
+
+    const searchFilter = (pokemonList: any) => {
+        return pokemonList.filter((pokemon: any) => 
+            pokemon.name.toLowerCase().includes(searchText.toLowerCase()))        
+    }
+
+    const filteredPokemonList = searchFilter(pokemonList);
+
+    return (
+        <>  
+        <div>   
+            <h3 className="text-2xl py-6 text-center">Pokemon Ara!</h3>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="pokemonName">Pokemon Adı</Label>
+                <Input 
+                    type="text" 
+                    value={searchText} 
+                    autoComplete="off"
+                    id="pokemonName" 
+                    placeholder="Pikachu, Ginger, Onix.." 
+                    onChange={(e) => setSearchText(e.target.value)} />
+            </div>
+            <h3 className="text-3xl pt-12 pb-6 text-center">Pokemonlar</h3>
+        </div>
+        
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+            {filteredPokemonList.map((pokemon: any) => {
+                return (
+                    <PokemonCard name={pokemon.name} key={pokemon.name + "Card"}/>
+                )
+            })}
+        </div>
+        </>
+    )
+}
