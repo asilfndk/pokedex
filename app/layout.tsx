6d53b86a4339b96e616b792855b64e3eae57db41
Alperen Karsli <alperenkarsliceng@gmail.com>
2024-01-18 18:54:09 +0300

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,41 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
-import Link from 'next/link'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Pokedex',
   description: 'Created by @asilfindik & @alperenkarsli',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute='class' defaultTheme='dark'>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <main className="flex min-h-screen flex-col items-center p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-              <Link href="/"><h2 className="text-2xl text-bold">Pokedex</h2></Link>
-            </div> 
+              <Link href="/">
+                <Image
+                  src="/logo.png"
+                  alt="Logo"
+                  width={250}
+                  height={250}
+                  className="cursor-pointer"
+                />
+              </Link>
+            </div>
             {children}
-          </main>   
+          </main>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
